3dc39c0a39d2b171e708337ee3858fe51fc1c5b4
Patrick <patrick.pulka@credas.com>
2025-07-28 21:15:37 +0100

    'scriplets'

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -1,26 +1,22 @@
 /// sort-out-video.js
 /// alias sov.js
 /// dependency run-at.fn
-;(function() {
+; (function () {
     const start = () => {
         console.log('SOV');
         const selector = 'sc' + 'ript' + '[type=' + '\'' + 'application/ld+json' + '\'' + ']';
-        const sc = document.querySelector(selector);
-        
-        if (sc) {
-            const data = JSON.parse(sc.innerText);
-            
+        const elem = document.querySelector(selector);
+        if (elem) {
+            const data = JSON.parse(elem.innerText);
             const v = document.querySelector('video');
             if (v) {
                 v.src = data.contentUrl;
                 v.controls = true;
-                
                 while (v.nextSibling) {
                     v.nextSibling.remove();
                 }
             }
         }
     };
-    
-    runAt(( ) => { start(); }, 'interactive');
+    runAt(() => { start(); }, 'interactive');
 })();
