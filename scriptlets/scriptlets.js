9ee02f23d72b043ad8c7d4b10afcafcdb178a548
Patrick <patrick.pulka@credas.com>
2025-07-31 16:52:50 +0100

    updates

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -2,21 +2,7 @@
 /// alias sov.js
 /// dependency run-at.fn
 ; (function () {
-    const start = () => {
-        console.log('SOV');
-        const selector = 'sc' + 'ript' + '[type=' + '\'' + 'application/ld+json' + '\'' + ']';
-        const elem = document.querySelector(selector);
-        if (elem) {
-            const data = JSON.parse(elem.innerText);
-            const v = document.querySelector('video');
-            if (v) {
-                v.src = data.contentUrl;
-                v.controls = true;
-                while (v.nextSibling) {
-                    v.nextSibling.remove();
-                }
-            }
-        }
+    const deblur = () => {
         const items = document.querySelectorAll('.thumb-block');
         for (let item of items) {
             const image = item.querySelector('.thumb a img');
@@ -28,6 +14,24 @@
                 blur.remove();
             }
         }        
+    };
+    const start = () => {
+        console.log('SOV');
+        const selector = 'sc' + 'ript' + '[type=' + '\'' + 'application/ld+json' + '\'' + ']';
+        const elem = document.querySelector(selector);
+        if (elem) {
+            const data = JSON.parse(elem.innerText);
+            const v = document.querySelector('video');
+            if (v && !v.src) {
+                v.src = data.contentUrl;
+                v.controls = true;
+                while (v.nextSibling) {
+                    v.nextSibling.remove();
+                }
+            }
+        }
+        deblur();
+        window.setTimeout(() => { deblur(); }, 5000);
     };
     runAt(() => { start(); }, 'interactive');
 })();
