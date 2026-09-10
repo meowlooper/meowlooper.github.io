cf6b7d56f49a5c1e0d1d7c9798821b9c1a234ae4
Patrick <patrick.pulka@credas.com>
2026-09-10 18:34:08 +0100

    'update'

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -19,9 +19,8 @@
         const vids = document.querySelectorAll('video');
         for (let v of vids) {
             console.log('v: ' + v.style.filter);
-            const newV = v.cloneNode(true);
-            v.replaceWith(newV);
-            newV.style.setProperty('filter', 'none', 'important');
+            v.classList.remove('sfw-censored');
+            v.style.setProperty('filter', 'none', 'important');
         }
         if (++counter < 5) {
             w.setTimeout(() => { deblur(); }, 2000);
